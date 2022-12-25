6a5efa008c9cb7c09f0acc60c84b0ce7c9c93893
JimenaGitHub <jaliagasanhueza@gmail.com>
2022-12-25 19:02:50 -0300

    Aplicando JQuery-botón Whatsapp

--- js/date.js
+++ js/date.js
@@ -9,3 +9,18 @@ const container = document.getElementById('task-container');
 //imprimo la fecha en el contenedor
 console.log('<p>' + date  + '</p>');
 container.innerHTML = '<p>' + date  + '</p>' ;
+
+
+// BOTON WHATSAPP
+$('#myDiv').floatingWhatsApp({
+    phone: '56993443055',
+    popupMessage: '¿Hola, cómo podemos ayudarte?',
+    showPopup: true,
+    position:'right',
+});
+
+// Funcion para hora
+setInterval(() => {
+    // console.log('ejecucion setTimeOut',new Date())
+    $('#date').html(new Date())    
+  }, 1000)
